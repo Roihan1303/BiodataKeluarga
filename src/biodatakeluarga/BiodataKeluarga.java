e91432d079a1e8f7d0c3af75ec7335797abed8f7
ASUS <ASUS@DESKTOP-VTOL558>
2018-09-19 13:46:57 +0700

    Final Commit

---
+++ src/biodatakeluarga/BiodataKeluarga.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodatakeluarga;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BiodataKeluarga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Program biodata = new Program();
+        biodata.setNamaAyah("Hermawan Setyadi");
+        biodata.setNamaIbu("Dieta Fauziyah");
+        biodata.setNamaSaudara("Favian Reswara Setyadi");
+        biodata.setNamaSaya("Roihan Farras Setyadi");
+        biodata.setAlamat("Jl Danau Ranau XII G6/D1");
+        biodata.setTempatLahir("Probolinggo");
+        biodata.setHobi("Olahraga");
+        biodata.setCitaCita("Programmer");
+        biodata.setNoTelepon("081294628199");
+        
+        System.out.println("==== BIODATA KELUARGA ====");
+        System.out.println();
+        System.out.println("Nama Ayah       : " + biodata.getNamaAyah());
+        System.out.println("Nama Ibu        : " + biodata.getNamaIbu());
+        System.out.println("Nama Saudara    : " + biodata.getNamaSaudara());
+        System.out.println("Nama Saya       : " + biodata.getNamaSaya());
+        System.out.println("Alamat          : " + biodata.getAlamat());
+        System.out.println("Tempat Lahir    : " + biodata.getTempatLahir());
+        System.out.println("Hobi            : " + biodata.getHobi());
+        System.out.println("Cita-Cita       : " + biodata.getCitaCita());
+        System.out.println("No Telepon      : " + biodata.getNoTelepon());
+        
+    }
+    
+}
---
+++ src/biodatakeluarga/Program.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodatakeluarga;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Program {
+    private String namaAyah;
+    private String namaIbu;
+    private String namaSaudara;
+    private String namaSaya;
+    private String alamat;
+    private String tempatLahir;
+    private String hobi;
+    private String citaCita;
+    private String noTelepon;
+
+    public String getNamaAyah() {
+        return namaAyah;
+    }
+
+    public String getNamaIbu() {
+        return namaIbu;
+    }
+
+    public String getNamaSaudara() {
+        return namaSaudara;
+    }
+
+    public String getNamaSaya() {
+        return namaSaya;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public String getHobi() {
+        return hobi;
+    }
+
+    public String getCitaCita() {
+        return citaCita;
+    }
+
+    public String getNoTelepon() {
+        return noTelepon;
+    }
+
+    public void setNamaAyah(String newNamaAyah) {
+        namaAyah = newNamaAyah;
+    }
+
+    public void setNamaIbu(String newNamaIbu) {
+        namaIbu = newNamaIbu;
+    }
+
+    public void setNamaSaudara(String newNamaSaudara) {
+        namaSaudara = newNamaSaudara;
+    }
+
+    public void setNamaSaya(String newNamaSaya) {
+        namaSaya = newNamaSaya;
+    }
+
+    public void setAlamat(String newAlamat) {
+        alamat = newAlamat;
+    }
+
+    public void setTempatLahir(String newTempatLahir) {
+        tempatLahir = newTempatLahir;
+    }
+
+    public void setHobi(String newHobi) {
+        hobi = newHobi;
+    }
+
+    public void setCitaCita(String newCitaCita) {
+        citaCita = newCitaCita;
+    }
+
+    public void setNoTelepon(String newNoTelepon) {
+        noTelepon = newNoTelepon;
+    }
+    
+    
+}
